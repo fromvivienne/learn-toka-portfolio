22d44797bf3d300291fc87543baf8774fed60bf3
fromvivienne <fromvivienne@me.com>
2023-05-01 21:39:35 +0900

    0501

--- src/views/Interviewer.vue
+++ src/views/Interviewer.vue
@@ -25,10 +25,36 @@
     <div id="modal-bis" class="modal">
       <div class="modal-background" @click="hide"></div>
       <div class="modal-content">
+<<<<<<< Updated upstream
         <p class="image is-4by3">
           <img src="https://bulma.io/images/placeholders/1280x960.png" alt="">
       
         </p>
+=======
+          <div class="card">
+            <div class="card-image">
+              <figure class="image is-4by3">
+                <img class="image" src="../assets/gallery/ree-fen.jpg" alt="">
+              </figure>
+            </div>
+            <div class="card-content">
+              <div class="media">
+                <div class="media-content">
+                  <p class="title is-4">作品名</p>
+                </div>
+              </div>
+
+              <div class="content">
+                ここに画の説明文を入れていけばいいと思います
+                <br>
+                <a>@toka</a>.
+                <a href="#">#art</a> <a href="#">#tag</a>
+                <br>
+                <time datetime="2016-1-1">2023/4/30</time>
+              </div>
+            </div>
+          </div>
+>>>>>>> Stashed changes
       </div>
     </div>
   </div>
